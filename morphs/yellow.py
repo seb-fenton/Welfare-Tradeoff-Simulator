407ea6bdd67c76fa240129ca1ac3ffdfe9491f1f
seb-fenton <seb.fenton@gmail.com>
2019-10-04 20:49:45 +0100

    +fixed yellow

--- morphs/yellow.py
+++ morphs/yellow.py
@@ -2,5 +2,5 @@ from .morph import Morph
 
 class YellowMorph(Morph):
     def makeChoice(self, target, config):
-        if(target.getColour() == config.colourMapping['yellow']): return 1
+        if(isinstance(target, YellowMorph)): return 1
         else: return 0
