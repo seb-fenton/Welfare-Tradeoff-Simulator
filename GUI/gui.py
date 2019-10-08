7506f2a2575195fb675a21944eddbb57aebf8121
seb-fenton <seb.fenton@gmail.com>
2019-10-08 11:37:32 +0100

    +gui memory

--- GUI/gui.py
+++ GUI/gui.py
@@ -6,6 +6,7 @@ def readToConfig(values):
     fileName = 'config.txt'
     fileList = []
 
+    #TODO:- do we need this?
     if(values[2] != None): fileName = values[2]
 
 
@@ -66,57 +67,128 @@ def readToConfig(values):
 
     return
 
-#sg.ChangeLookAndFeel('GreenTan')      
+def readFromConfig():
+    fileName = 'config.txt'
+    fileList = []
 
-# ------ Menu Definition ------ #      
-menu_def = [['File', ['Exit']],      
-            ['Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],      
-            ['Help', 'About...'], ]      
+    try:      
+        with open(fileName) as f_obj:
+            for line in f_obj:
+                fileList.append(line)
+    except FileNotFoundError:
+        msg = "Can't find file {0}.".format(fileName)
+        print(msg)
+        exit
 
-# ------ Column Definition ------ #        
+    inputs = {}
 
-sg.SetOptions(text_justification='right')      
+    ###GENERAL###
 
-layout = [[sg.Text('Welfare Tradeoffs', font=('Helvetica', 16))],      
-          [sg.Text('Turns', size=(15, 1)), sg.Spin(values=[i for i in range(1, 1000)], initial_value=20, size=(6, 1)),      
-           sg.Text('Games', size=(18, 1)), sg.Spin(values=[i for i in range(1, 1000)], initial_value=1, size=(6, 1))],      
-          [sg.Text('Input Folder', size=(15, 1)), sg.In(default_text='config.txt', size=(10, 1)), sg.Text('Selfish Reward', size=(15, 1)),      
-           sg.In(default_text='4', size=(10, 1))],      
-          [sg.Text('Starting Energy', size=(15, 1)), sg.In(default_text='8', size=(10, 1)), sg.Text('Selfless Reward', size=(15, 1)),      
-           sg.In(default_text='6', size=(10, 1))],      
-          [sg.Text('Tax', size=(15, 1)), sg.In(default_text='4', size=(10, 1)), sg.Text('Mutation Probability', size=(15, 1)),      
-           sg.In(default_text='0', size=(10, 1))],
-          [sg.Text('_'  * 100, size=(65, 1))], 
-          [sg.Text('Multi-game options',font=('Helvetica', 15), justification='left')],
-          [sg.Text('Pop. Increment', size=(15, 1)), sg.In(default_text='0', size=(10, 1)), sg.Text('Turn Increment', size=(15, 1)),      
-           sg.In(default_text='0', size=(10, 1))],          
-          [sg.Text('_'  * 100, size=(65, 1))],      
-          [sg.Text('Toggles', font=('Helvetica', 15), justification='left')],      
-          [sg.Checkbox('Random Distribution', size=(12, 1), default=False), sg.Checkbox('Formidability', size=(20, 1))],      
-          [sg.Checkbox('Plot individual games', size=(12, 1), default=True), sg.Checkbox('N/A', size=(20, 1), default=True)],      
-          [sg.Checkbox('Save results', default=True, size=(12, 1)), sg.Checkbox('N/A', size=(20, 1))],      
-          [sg.Text('_'  * 100, size=(65, 1))],      
-          [sg.Text('Starting Populations', font=('Helvetica', 15), justification='left')],      
-          [sg.Text('Random', size=(15, 1)), sg.In(default_text='20', size=(10, 1)),      
-           sg.Text('Reciprocal', size=(15, 1)), sg.In(default_text='20', size=(10, 1))],      
-          [sg.Text('Selfish', size=(15, 1)), sg.In(default_text='20', size=(10, 1)),
-           sg.Text('Selfless', size=(15, 1)), sg.In(default_text='20', size=(10, 1))],      
-          [sg.Text('Kin-selective', size=(15, 1)), sg.In(default_text='20', size=(10, 1)),      
-           sg.Text('Reactionary', size=(15, 1)), sg.In(default_text='20', size=(10, 1))],          
-          [sg.Submit(), sg.Cancel()]]      
+    #Turns
+    inputs['Turns'] = fileList[4].rstrip()
+    #Starting Energy
+    inputs['Starting energy'] = fileList[6].rstrip()
+    #Tax
+    inputs['Tax'] = fileList[8].rstrip()
+    #Selfish reward
+    inputs['Selfish reward'] = fileList[40].rstrip()
+    #Selfless reward
+    inputs['Selfless reward'] = fileList[42].rstrip()
+    #Mutation probability
+    inputs['Mutation probability'] = fileList[46].rstrip()
 
-window = sg.Window('Welfare Tradeoffs', layout, font=("Helvetica", 12))
+    #Random Dist
+    if(fileList[44].rstrip() == str(1)): inputs['Random distribution'] = True
+    else: inputs['Random distribution'] = False
+    #Plot individual
+    if(fileList[32].rstrip() == str(1)): inputs['Plot individual'] = True
+    else: inputs['Plot individual'] = False
+    #Save results
+    if(fileList[36].rstrip() == str(1)): inputs['Save results'] = True
+    else: inputs['Save results'] = False
 
-# Event Loop to process "events" and get the "values" of the inputs
-while True:             
-    event, values = window.read()
-    if event in (None, 'Cancel'):   # if user closes window or clicks cancel
-        window.close()
-        break
-    elif event in ('Submit'):
-        readToConfig(values)
-        os.system('python3 main.py')
+    ###POPS###
 
+    #Selfish Red
+    inputs['Redpop'] = fileList[12].rstrip()
+    #Random Blue
+    inputs['Bluepop'] = fileList[14].rstrip() 
+    #Reciprocal Green
+    inputs['Greenpop'] = fileList[16].rstrip() 
+    #Selfless Pink
+    inputs['Pinkpop'] = fileList[18].rstrip() 
+    #Kin-selective Yellow
+    inputs['Yellowpop'] = fileList[20].rstrip()
+    #Reactive White
+    inputs['Whitepop'] = fileList[22].rstrip()
+
+    return inputs
+
+def Gui(inputs):
+
+    #sg.ChangeLookAndFeel('GreenTan')      
+
+    # ------ Menu Definition ------ #      
+    menu_def = [['File', ['Exit']],      
+                ['Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],      
+                ['Help', 'About...'], ]      
+
+    # ------ Column Definition ------ #        
+
+    sg.SetOptions(text_justification='right')      
+
+    layout = [[sg.Text('Welfare Tradeoffs', font=('Helvetica', 16))],      
+            [sg.Text('Turns', size=(15, 1)), sg.Spin(values=[i for i in range(1, 1000)], initial_value=inputs['Turns'], size=(6, 1)),      
+            sg.Text('Games', size=(18, 1)), sg.Spin(values=[i for i in range(1, 1000)], initial_value=1, size=(6, 1))],      
+            [sg.Text('Input Folder', size=(15, 1)), sg.In(default_text='config.txt', size=(10, 1)), sg.Text('Selfish Reward', size=(15, 1)),      
+            sg.In(default_text=inputs['Selfish reward'], size=(10, 1))],      
+            [sg.Text('Starting Energy', size=(15, 1)), sg.In(default_text=inputs['Starting energy'], size=(10, 1)), sg.Text('Selfless Reward', size=(15, 1)),      
+            sg.In(default_text=inputs['Selfless reward'], size=(10, 1))],      
+            [sg.Text('Tax', size=(15, 1)), sg.In(default_text=inputs['Tax'], size=(10, 1)), sg.Text('Mutation Probability', size=(15, 1)),      
+            sg.In(default_text=inputs['Mutation probability'], size=(10, 1))],
+            [sg.Text('_'  * 100, size=(65, 1))], 
+            [sg.Text('Multi-game options',font=('Helvetica', 15), justification='left')],
+            [sg.Text('Pop. Increment', size=(15, 1)), sg.In(default_text='0', size=(10, 1)), sg.Text('Turn Increment', size=(15, 1)),      
+            sg.In(default_text='0', size=(10, 1))],          
+            [sg.Text('_'  * 100, size=(65, 1))],      
+            [sg.Text('Toggles', font=('Helvetica', 15), justification='left')],      
+            [sg.Checkbox('Random Distribution', size=(12, 1), default=inputs['Random distribution']), sg.Checkbox('Formidability', size=(20, 1))],      
+            [sg.Checkbox('Plot individual games', size=(12, 1), default=inputs['Plot individual']), sg.Checkbox('Placeholder', size=(20, 1), default=True)],      
+            [sg.Checkbox('Save results', default=inputs['Save results'], size=(12, 1)), sg.Checkbox('Placeholder', size=(20, 1))],      
+            [sg.Text('_'  * 100, size=(65, 1))],      
+            [sg.Text('Starting Populations', font=('Helvetica', 15), justification='left')],      
+            [sg.Text('Random', size=(15, 1)), sg.In(default_text=inputs['Bluepop'], size=(10, 1)),      
+            sg.Text('Reciprocal', size=(15, 1)), sg.In(default_text=inputs['Greenpop'], size=(10, 1))],      
+            [sg.Text('Selfish', size=(15, 1)), sg.In(default_text=inputs['Redpop'], size=(10, 1)),
+            sg.Text('Selfless', size=(15, 1)), sg.In(default_text=inputs['Pinkpop'], size=(10, 1))],      
+            [sg.Text('Kin-selective', size=(15, 1)), sg.In(default_text=inputs['Yellowpop'], size=(10, 1)),      
+            sg.Text('Reactionary', size=(15, 1)), sg.In(default_text=inputs['Whitepop'], size=(10, 1))],          
+            [sg.Submit(), sg.Cancel()]]      
+
+    window = sg.Window('Welfare Tradeoffs', layout, font=("Helvetica", 12))
+
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:             
+        event, values = window.read()
+        if event in (None, 'Cancel'):   # if user closes window or clicks cancel
+            window.close()
+            break
+        elif event in ('Submit'):
+            readToConfig(values)
+            os.system('python3 main.py')
+
+
+def main():
+
+    inputs = readFromConfig()
+    Gui(inputs)
+
+    return
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
